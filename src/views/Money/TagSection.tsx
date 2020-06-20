f0afc2f47a200ac0c769bd3d8fd56fa9bc3290b6
lm <710450868@qq.com>
2020-06-20 12:30:21 +0800

    完成新增标签和标签的选中变色

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-const TagsSection = styled.section`
+import React, {useState} from 'react';
+
+const Wrapper = styled.section`
      background:#FFFFFF;
      padding: 12px 16px;
      flex-grow: 1;
@@ -16,7 +18,11 @@ const TagsSection = styled.section`
          display: inline-block;
          padding: 3px 18px;
          font-size: 14px;
-         margin: 8px 12px;   
+         margin: 8px 12px;  
+         &.selected{
+           background:coral;
+           color: white;
+         } 
         }
      }
      > button{
@@ -28,4 +34,34 @@ const TagsSection = styled.section`
          margin-top: 8px;
      }
 `;
+const TagsSection:React.FC=(props)=>{
+  const[tags,setTags]=useState<string[]>(['衣','食','住','行']);
+  const [selectedTags,setSelectedTags]=useState<string[]>([])
+  const onAddTag=()=>{
+    const tagName=window.prompt('请输入标签名');
+    if(tagName!==null){
+      setTags([...tags,tagName])
+    }
+  };
+  const onToggleTag=(tag: string)=>{
+    const index =selectedTags.indexOf(tag);
+    if(index>=0){
+      setSelectedTags(selectedTags.filter(t=>t!==tag));
+    }else {
+      setSelectedTags([...selectedTags,tag])
+    }
+  };
+  const getClass=(tag: string)=>selectedTags.indexOf(tag)>=0?'selected':'';
+  return(
+    <Wrapper>
+      <ol>
+        {tags.map(tag=>
+          <li key={tag} onClick={()=>onToggleTag(tag)}
+          className={getClass(tag)}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 export {TagsSection};
