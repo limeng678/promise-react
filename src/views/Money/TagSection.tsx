d12d8f7facf4d64dbf0b2bf88f711673618eb9ba
lm <710450868@qq.com>
2020-06-21 10:54:21 +0800

    重构：将tag:string重构为tag:{id:number;name:string}

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -37,34 +37,34 @@ const Wrapper = styled.section`
 `;
 
 
-type Props ={value:string[];
-             onChange: (value: string[])=>void;
+type Props ={value:number[];
+             onChange: (value: number[])=>void;
 }
 
 const TagsSection:React.FC<Props> =(props)=>{
   const{tags,setTags}=useTags();
-  const selectedTags=props.value
+  const selectedTagIds=props.value
   const onAddTag=()=>{
     const tagName=window.prompt('请输入标签名');
     if(tagName!==null){
-      setTags([...tags,tagName])
+      setTags([...tags,{id:Math.random(),name:tagName}]);
     }
   };
-  const onToggleTag=(tag: string)=>{
-    const index =selectedTags.indexOf(tag);
+  const onToggleTag=(tagId: number)=>{
+    const index =selectedTagIds.indexOf(tagId);
     if(index>=0){
-      props.onChange(selectedTags.filter(t=>t!==tag));
+      props.onChange(selectedTagIds.filter(t=>t!==tagId));
     }else {
-      props.onChange([...selectedTags,tag])
+      props.onChange([...selectedTagIds,tagId])
     }
   };
-  const getClass=(tag: string)=>selectedTags.indexOf(tag)>=0?'selected':'';
+  const getClass=(tagId: number)=>selectedTagIds.indexOf(tagId)>=0?'selected':'';
   return(
     <Wrapper>
       <ol>
         {tags.map(tag=>
-          <li key={tag} onClick={()=>onToggleTag(tag)}
-          className={getClass(tag)}>{tag}</li>
+          <li key={tag.id} onClick={()=>onToggleTag(tag.id)}
+          className={getClass(tag.id)}>{tag.name}</li>
         )}
       </ol>
       <button onClick={onAddTag}>新增标签</button>
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -38,14 +38,14 @@ const Space=styled.div`
     height: 32px;
 `
 function Tags() {
-  const{tags,setTags}=useTags();
+  const{tags}=useTags();
   return(
     <Layout>
 
       <TagList>
-        {tags.map(tag=><li key={tag}>
+        {tags.map(tag=><li key={tag.id}>
                        <Link to={'/tags/'+tag}>
-                       <span className="oneLine">{tag}</span>
+                       <span className="oneLine">{tag.name}</span>
                        <Icon name="right"/>
                        </Link>
                        </li>)}
