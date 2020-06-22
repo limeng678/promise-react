f192aa2779d49b9bf2bc1b3e490ab50bf9ad7de4
lm <710450868@qq.com>
2020-06-22 23:09:15 +0800

    解决无法输入.的bug

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Wrapper} from './NumberPadSection/Wrapper';
 import {generateOutput} from './NumberPadSection/generateOutput';
-
 
 type Props = {
   value: number;
@@ -9,17 +8,18 @@ type Props = {
   onOk?: () => void;
 }
 const NumberPadSection: React.FC<Props> = (props) => {
-  const output = props.value.toString();
-  const setOutput = (output: string) => {
-    let value;
+   const [output,_setOutput] =useState(props.value.toString())
+   const setOutput = (output: string) => {
+    let newOutput:string;
     if (output.length > 16) {
-      value = parseFloat(output.slice(0, 16));
+      newOutput = output.slice(0, 16);
     } else if (output.length === 0) {
-      value = 0;
+      newOutput = '0';
     } else {
-      value = parseFloat(output);
+      newOutput = output;
     }
-    props.onChange(value);
+    _setOutput(newOutput);
+    props.onChange(parseFloat(newOutput));
   };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
@@ -57,6 +57,5 @@ const NumberPadSection: React.FC<Props> = (props) => {
     </Wrapper>
   );
 };
-
 
 export {NumberPadSection};
