661a9b4017076df96a71185bbf6cf3dae45afce5
lm <710450868@qq.com>
2020-06-23 15:34:49 +0800

    消除警告

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -33,7 +33,7 @@ function Statistics() {
   const {getName}=useTags();
   const hash: {[k: string] : RecordItem[]}={}
   const selectedRecords=records.filter(r=>r.category===category);
-  selectedRecords.map(r=>{
+  selectedRecords.forEach(r=>{
     const key =day(r.createdAt).format('YYYY-MM-DD')
     if(!(key in hash)){
       hash[key]=[]
